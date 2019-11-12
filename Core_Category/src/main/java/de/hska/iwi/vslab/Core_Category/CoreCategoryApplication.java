aa16d0a04810cf3e4c6939f066db0de3367884d8
and1kl <40032530+and1kl@users.noreply.github.com>
2019-11-12 18:36:22 +0100

    Spring Boot Project Initial

---
+++ Core_Category/src/main/java/de/hska/iwi/vslab/Core_Category/CoreCategoryApplication.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.Core_Category;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreCategoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreCategoryApplication.class, args);
+	}
+
+}
---
+++ Core_Category/src/test/java/de/hska/iwi/vslab/Core_Category/CoreCategoryApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.Core_Category;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreCategoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
